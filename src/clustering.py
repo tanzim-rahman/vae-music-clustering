90108ee998dc6b77a65e4abc76699a5c98e48228
Tanzim Rahman <tanzimrahman@gmail.com>
2026-04-20 12:18:46 +0600

    remove old files

--- src/clustering.py
+++
@@ -1,7 +0,0 @@
-from sklearn.cluster import KMeans, AgglomerativeClustering
-
-def kmeans(X, k=6):
-    return KMeans(n_clusters=k, random_state=27).fit_predict(X)
-
-def agglo(X, k=6):
-    return AgglomerativeClustering(n_clusters=k).fit_predict(X)
--- src/fusion.py
+++
@@ -1,7 +0,0 @@
-import numpy as np
-from sklearn.preprocessing import StandardScaler
-
-def fuse(z_audio, X_lyrics, weight=0.5):
-    z_audio = StandardScaler().fit_transform(z_audio)
-    X_lyrics = StandardScaler().fit_transform(X_lyrics)
-    return np.concatenate([z_audio, weight*X_lyrics], axis=1)
