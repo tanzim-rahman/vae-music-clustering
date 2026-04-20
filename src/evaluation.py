3250446915df19089b12ab807c10acede763b5f9
Tanzim Rahman <tanzimrahman@gmail.com>
2026-04-20 15:21:35 +0600

    added medium task evaluation

--- src/evaluation.py
+++ src/evaluation.py
@@ -1,6 +1,6 @@
 from sklearn.preprocessing import LabelEncoder
-from sklearn.cluster import KMeans
-from sklearn.metrics import silhouette_score, calinski_harabasz_score, adjusted_rand_score
+from sklearn.cluster import KMeans, AgglomerativeClustering
+from sklearn.metrics import silhouette_score, calinski_harabasz_score, davies_bouldin_score, adjusted_rand_score
 
 def evaluation_easy(Z, true_labels, n_clusters):
     # Encode labels to integers
@@ -17,3 +17,30 @@ def evaluation_easy(Z, true_labels, n_clusters):
     ari = adjusted_rand_score(y_true, y_pred)
 
     return kmeans, y_pred, sil, ch, ari
+
+def evaluation_medium(Z, true_labels, n_clusters):
+    # Encode labels to integers
+    le = LabelEncoder()
+    y_true = le.fit_transform(true_labels)
+
+    # KMeans
+    kmeans = KMeans(n_clusters=n_clusters, random_state=27).fit(Z)
+    y_pred_kmeans = kmeans.labels_
+
+    # Metrics
+    sil_kmeans = silhouette_score(Z, y_pred_kmeans)
+    ch_kmeans = calinski_harabasz_score(Z, y_pred_kmeans)
+    d_kmeans = davies_bouldin_score(Z, y_pred_kmeans)
+    ari_kmeans = adjusted_rand_score(y_true, y_pred_kmeans)
+
+    # Agglo
+    agglo = AgglomerativeClustering(n_clusters=n_clusters).fit(Z)
+    y_pred_agglo = agglo.labels_
+
+    # Metrics
+    sil_agglo = silhouette_score(Z, y_pred_agglo)
+    ch_agglo = calinski_harabasz_score(Z, y_pred_agglo)
+    d_agglo = davies_bouldin_score(Z, y_pred_agglo)
+    ari_agglo = adjusted_rand_score(y_true, y_pred_agglo)
+
+    return kmeans, y_pred_kmeans, agglo, y_pred_agglo, sil_kmeans, ch_kmeans, d_kmeans, ari_kmeans, sil_agglo, ch_agglo, d_agglo, ari_agglo
