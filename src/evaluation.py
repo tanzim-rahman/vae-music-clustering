97c26dc36ca12acab9b30652c39c8b7aae8d4a49
Tanzim Rahman <tanzimrahman@gmail.com>
2026-04-20 12:34:39 +0600

    Added evaluations for easy task

--- src/evaluation.py
+++ src/evaluation.py
@@ -1,50 +1,19 @@
-import numpy as np
-from sklearn.metrics import (
-    silhouette_score,
-    calinski_harabasz_score,
-    davies_bouldin_score,
-    adjusted_rand_score,
-    normalized_mutual_info_score
-)
+from sklearn.preprocessing import LabelEncoder
+from sklearn.cluster import KMeans
+from sklearn.metrics import silhouette_score, calinski_harabasz_score, adjusted_rand_score
 
-def cluster_purity(true_labels, pred_labels):
-    clusters = np.unique(pred_labels)
-    total = len(true_labels)
-    purity_sum = 0
+def evaluation_easy(Z, true_labels, n_clusters):
+    # Encode labels to integers
+    le = LabelEncoder()
+    y_true = le.fit_transform(true_labels)
 
-    for cluster in clusters:
-        # ignore noise from DBSCAN
-        # not used in this project
-        if cluster == -1:
-            continue
+    # KMeans
+    kmeans = KMeans(n_clusters=n_clusters, random_state=27).fit(Z)
+    y_pred = kmeans.labels_
 
-        # data points in the cluster
-        indices = np.where(pred_labels == cluster)[0]
+    # Metrics
+    sil = silhouette_score(Z, y_pred)
+    ch = calinski_harabasz_score(Z, y_pred)
+    ari = adjusted_rand_score(y_true, y_pred)
 
-        if len(indices) == 0:
-            continue
-
-        # true labels of data points
-        true_cluster_labels = true_labels[indices]
-
-        # true label frequencies
-        counts = np.bincount(true_cluster_labels)
-
-        # add dominant class to purity
-        purity_sum += np.max(counts)
-
-    # returns fraction of dominant classes
-    return purity_sum / total
-
-def evaluate_clustering(X, labels, true_labels):
-    results = {}
-
-    results["Silhouette"] = silhouette_score(X, labels)
-    results["Calinski"] = calinski_harabasz_score(X, labels)
-    results["Davies"] = davies_bouldin_score(X, labels)
-
-    results["ARI"] = adjusted_rand_score(true_labels, labels)
-    results["NMI"] = normalized_mutual_info_score(true_labels, labels)
-    results["Purity"] = cluster_purity(true_labels, labels)
-
-    return results
+    return kmeans, y_pred, sil, ch, ari
