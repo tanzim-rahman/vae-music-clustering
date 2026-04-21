a81717c37cab3845e8bc05a3b9a4b0a68564b683
Tanzim Rahman <tanzimrahman@gmail.com>
2026-04-21 13:09:09 +0600

    added cluster dist and reconstruction visualisations

--- src/visualisation.py
+++ src/visualisation.py
@@ -1,8 +1,11 @@
 from sklearn.preprocessing import LabelEncoder
 from sklearn.manifold import TSNE
 
+import pandas as pd
+
 import matplotlib.pyplot as plt
 from matplotlib import cm
+import seaborn as sns
 
 def plot_training_loss(loss_history, save_path=None):
     plt.figure()
@@ -45,4 +48,58 @@ def plot_tsne(Z, labels, title, filename, cmap_name='tab10'):
 
     plt.title(title)
     plt.savefig(filename, bbox_inches='tight')
+    plt.show()
+
+def plot_cluster_distribution(df, title, save_path=None, cmap='tab10'):
+    # Count occurrences
+    cluster_dist = pd.crosstab(df['cluster'], df['label'])
+
+    # Normalise (proportions)
+    cluster_dist_norm = cluster_dist.div(cluster_dist.sum(axis=1), axis=0)
+
+    # Plot
+    cluster_dist_norm.plot(
+        kind='bar',
+        stacked=True,
+        figsize=(8,6),
+        colormap=cmap
+    )
+
+    plt.title(title)
+    plt.xlabel('Cluster')
+    plt.ylabel('Proportion')
+    plt.legend(title='Label', bbox_to_anchor=(1.05, 1))
+    plt.tight_layout()
+    if save_path != None:
+        plt.savefig(f'{save_path}.svg')
+    plt.show()
+
+    plt.figure(figsize=(8,6))
+    sns.heatmap(
+        cluster_dist_norm,
+        annot=True,
+        cmap='Blues',
+        fmt='.2f',
+        cbar=False
+    )
+
+    plt.title('Cluster vs Label Distribution (Heatmap)')
+    plt.xlabel('Label')
+    plt.ylabel('Cluster')
+    plt.tight_layout()
+    plt.savefig(f'{save_path}_heatmap.svg')
+    plt.show()
+
+def plot_reconstruction(audio_orig, audio_recon, i, save_path=None):
+    fig, axs = plt.subplots(1, 2, figsize=(10,4))
+
+    axs[0].imshow(audio_orig[i][0], aspect='auto', origin='lower')
+    axs[0].set_title('Original')
+
+    axs[1].imshow(audio_recon[i][0], aspect='auto', origin='lower')
+    axs[1].set_title('Reconstructed')
+
+    plt.tight_layout()
+    if save_path != None:
+        plt.savefig(save_path)
     plt.show()
